9a82cdff4d420606522425b45ed67cd3a4766b60
kaushal1532 <kaushal.kerinfotech@gmail.com>
2021-12-03 19:22:14 +0530

    2 -day express js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-// const path = require("path");
+const path = require("path");
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
 
-// const staticpath = path.join(__dirname, "../public");
+const staticpath = path.join(__dirname, "../public");
+const templatepath = path.join(__dirname, "../templates/view");
+const partialspath = path.join(__dirname, "../templates/partials");
+
 //to set the view engine:-
-// app.set("view engine", "hbs");
+app.set("view engine", "hbs");
+app.set("views engine", templatepath);
+hbs.registerPartials(partialspath)
+
+// console.log(__dirname);
 app.use(express.static(staticpath));
 
 //template engine route:-
-// app.get("", (req, res) => {
-//     res.render('index');
-// })
+app.get("/", (req, res) => {
+    res.render("index");
 
+});
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+app.get("*/about/*", (req, res) => {
+    res.render("404", {
+        errorcomment: "one page couldn't be found ",
+    });
+});
+app.get("*", (req, res) => {
+    res.render("404", {
+        errorcomment: "one page couldn't be found ",
+    });
+});
 app.get("/", (req, res) => {
     res.send("jay mataji form the express.");
 });
